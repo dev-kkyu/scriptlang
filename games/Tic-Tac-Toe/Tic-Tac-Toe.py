9928ed21e31ae61d88273a0b153ef021d93e870b
임성규 <dev.kkyu@gmail.com>
2022-05-07 20:59:35 +0900

    IMG_button

--- games/Tic-Tac-Toe/Tic-Tac-Toe.py
+++ games/Tic-Tac-Toe/Tic-Tac-Toe.py
@@ -1,5 +1,23 @@
-from tkinter import *
+from tkinter import * #tkinter 모듈에 있는 모든 함수 포함
 
-window = Tk()
+window = Tk() #Gui 생성
 
-window.mainloop()
+window.title("Tic-Tac-Toe") #창이름설정
+window.geometry("300x330+100+100") #창사이즈설정
+window.resizable(True, True) #사이즈 변경 허용
+
+img_empty=PhotoImage(file="./image/empty.gif")
+img_x=PhotoImage(file="./image/x.gif")
+img_o=PhotoImage(file="./image/o.gif")
+
+label_empty=Button(window, image=img_empty)
+label_x=Button(window, image=img_x)
+label_o=Button(window, image=img_o)
+
+label_empty.pack()#side="top")
+label_x.pack()#side="bottom")
+label_o.pack()#side="right")
+
+
+window.mainloop() #윈도우 실행
+
