3d0c4e6346450ba28afabf3f6672dd5e8b5ebe98
박자은 <jjaeunjj@gmail.com>
2022-05-09 22:41:46 +0900

    Make Window

---
+++ main.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+window = Tk()  # Gui 생성
+window.title("Subway")  # 창이름설정
+window.geometry("1000x600+100+100")  # 창사이즈설정
+window.resizable(True,True)  # 사이즈 변경 허용
+
+
+window.mainloop()
