c3c45905e1f2cfbd03fcedb8d9fbc37a0e7b1773
임성규 <dev.kkyu@gmail.com>
2022-05-20 20:07:08 +0900

    Edit Display

--- main.py
+++ main.py
@@ -1,80 +1,87 @@
 from tkinter import *
 
 window = Tk()  # Gui 생성
-window.title("언제타 지하철")  # 창이름설정
-window.geometry("1000x600+100+100")  # 창사이즈설정
-window.resizable(True,True)  # 사이즈 변경 허용
+window.title("Subway")  # 창이름설정
+window.geometry("1050x600+100+100")  # 창사이즈설정
+window.resizable(True,False)  # 사이즈 변경 허용
 
-frameside = Frame(window, relief="flat", bd=6, bg="#00B050")
-frame = Frame(frameside, bd=0)
-
-frameside.pack(fill = "both", expand=True)
-frame.pack(fill = "both", expand=True)
-
-Title = Label(frame, text="지하철 시간표 API", font = ("맑은 고딕",30), width = 30, height = 5)
-Title.pack()
-
-frameside = Frame(window, relief="flat", bd=6, bg="#00B050")
-frame1 = Frame(frameside, bd=0)
-frame2 = Frame(frameside, bd=0)
-frame3 = Frame(frameside, bd=0)
-frame4 = Frame(frameside, bd=0)
+frameside = Frame(window, relief="flat", bd=6, bg="#00B050") # 테두리 전용 프레임
+Main = Frame(frameside, bd=0) # 메인창
 
 frameside.pack(fill = "both", expand=True)
-frame1.pack(fill = "both", expand=True)
-frame2.pack(fill = "both", expand=True)
-frame3.pack(fill = "both", expand=True)
-frame4.pack(fill = "both", expand=True)
-
-Title = Label(frame1, text="언제타 지하철", font = ("맑은 고딕",30), width = 30)
-Title.grid(row=0, column=0)
-
-input_text = Entry(frame1, width=30) 
-input_text.grid(row=0, column=1) 
-
-button = Button(frame1, text="확인") 
-button.grid(row=0, column=2)
+Main.pack(fill = "both", expand=True)
 
 
+frame1 = Frame(Main, relief="solid", bd=1)
+frame1.pack(fill="both")
 
-button1 = Button(frame2, text="1호선",width=12,height=4)
-button2 = Button(frame2, text="2호선",width=12,height=4)
-button3 = Button(frame2, text="3호선",width=12,height=4)
-button4 = Button(frame2, text="4호선",width=12,height=4)
-button5 = Button(frame2, text="5호선",width=12,height=4)
-button6 = Button(frame2, text="6호선",width=12,height=4)
-button7 = Button(frame2, text="7호선",width=12,height=4)
-button8 = Button(frame2, text="8호선",width=12,height=4)
-button9 = Button(frame2, text="9호선",width=12,height=4)
-button1.grid(row=0, column=0)
-button2.grid(row=0, column=1)
-button3.grid(row=0, column=2)
-button4.grid(row=0, column=3)
-button5.grid(row=0, column=4)
-button6.grid(row=0, column=5)
-button7.grid(row=0, column=6)
-button8.grid(row=0, column=7)
-button9.grid(row=0, column=8)
+frame1left = Frame(frame1, relief="solid", bd=1)
+frame1left.pack(fill="both", side="left")
 
-image=Label(frame2, text="N호선 사진",width=12,height=5,relief="solid")
-image.grid(row=0, column=9)
+frame1right = Frame(frame1, relief="solid", bd=1)
+frame1right.pack(fill="both", expand = True, side="left")
 
+Title = Label(frame1left, text="언제타 지하철", font = ("맑은 고딕",44, "bold"), height=1, width=13, padx = 10)
+Title.pack(fill = "both")
 
+empty = Label(frame1right, width = 1, bd=0)
+empty.pack(side = "left", fill = "both")
 
-label=Label(frame3, text="역목록", width=67, height=15,relief="solid")
-label.grid(row=0, column=0)
+textframe = Frame(frame1right, relief = "flat", bd=3, bg = "#64C044")
+textframe.pack(side = "left")
 
-label1=Label(frame3, text="시간표", width=67, height=15,relief="solid")
-label1.grid(row=0, column=1)
+input_text = Entry(textframe, width=50)
+input_text.pack(fill = "both", side = "left")
 
+button = Button(textframe, text="검색", font = ("맑은 고딕",15), height= 1, width = 7, bg = "#92D050", padx = 10)
+button.pack( side = "left")
 
-graph=Label(frame4, text="그래프", width=100, height=7,relief="solid")
-graph.grid(row=0, column=0)
+frame2 = Frame(Main, relief="solid", bd=1)
+frame2.pack(fill="both")
 
-email = Button(frame4, text="이메일",width=16,height=7)
-map = Button(frame4, text="지도",width=16,height=7)
+subwayLineButton = []
+emptyLabel = []
+for i in range(9):
+    if i==0:
+        emptyLabel.append(Label(frame2, width = 3, height = 5))
+    else:
+        emptyLabel.append(Label(frame2, width = 1))
+    subwayLineButton.append(Button(frame2, text=str(i+1)+"호선", font = ("맑은 고딕", 15, "bold"), width = 6, height = 1, fg = "white", bg = "#92D050"))
 
-email.grid(row=0, column=1)
-map.grid(row=0, column=2)
+    emptyLabel[i].pack(side = "left")
+    subwayLineButton[i].pack(side = "left")
+
+frame3 = Frame(Main, relief = "solid", bd=1)
+frame3.pack(fill="both")
+
+frame3left = Frame(frame3, relief = "solid", bd=1)
+frame3right = Frame(frame3, relief = "solid", bd=1)
+frame3left.pack(fill="both", side = "left")
+frame3right.pack(fill="both", side = "left", expand = True)
+
+subwaylist = Label(frame3left, text = "호선별 역 목록", width = 71, height = 2, relief = "solid", bd=1, bg = "#64C044")
+subwaytime = Label(frame3right, text = "역 열차 시간표", height = 2, relief = "solid", bd=1, bg = "#64C044")
+
+subwaylist.pack(fill = "both")
+subwaytime.pack(fill = "both")
+
+test1label = Label(frame3left, text = "left",  height = 16)
+test2label = Label(frame3right, text = "right")
+
+test1label.pack(fill = "both")
+test2label.pack(fill = "both")
+
+frame4 = Frame(Main, relief = "solid", bd=1)
+frame4.pack(fill="both", expand = True)
+
+graphframe = Frame(frame4, relief = "solid", bd=1, width = 750)
+graphframe.pack(fill = "both", side = "left")
+
+email = Button(frame4, width = 16)
+kakaomap = Button(frame4)
+
+email.pack(fill = "both", side = "left", padx = 10, pady = 10)
+kakaomap.pack(fill = "both", side = "left", expand = True, padx = 10, pady = 10)
 
 window.mainloop()
+
