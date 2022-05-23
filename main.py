0febf89fb0c20c97649875c8fb1cf6e8b992c885
임성규 <dev.kkyu@gmail.com>
2022-05-23 17:53:07 +0900

    Make Search Func

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 from tkinter import *
+import loadapi
 
 window = Tk()  # Gui 생성
 window.title("Subway")  # 창이름설정
 window.geometry("980x600+100+100")  # 창사이즈설정
 window.resizable(False, True)  # 사이즈 변경 허용
+
+def outputScheduld():
+   global lineimage
+   data = LoadSubwayTimetable(input_text.get())
+   listbox2.delete(0, END)
+   for i, x in enumerate(data['Schedule']['1']):
+      listbox2.insert(i, x)
+   lineimage = PhotoImage(file = "./image/"+data['LINE_NM']+".gif").subsample(50, 50)
+   linephoto.config(image = lineimage)
+   pass
+
+
 
 frameside = Frame(window, relief="flat", bd=6, bg="#00B050") # 테두리 전용 프레임
 Main = Frame(frameside, bd=0, bg = "#FFFFFF") # 메인창
@@ -27,10 +40,10 @@ Title.pack(fill = "both")
 textframe = Frame(frame1right, relief = "flat", bd=3, bg = "#64C044")
 textframe.pack(side = "left", padx = 20)
 
-input_text = Entry(textframe, width=50)
+input_text = Entry(textframe, width=30, font = ("맑은 고딕",15))
 input_text.pack(fill = "both", side = "left")
 
-button = Button(textframe, text="검색", font = ("맑은 고딕",15), height= 1, width = 7, bg = "#92D050", padx = 10)
+button = Button(textframe, text="검색", command = outputScheduld, font = ("맑은 고딕",15), height= 1, width = 7, bg = "#92D050", padx = 10)
 button.pack( side = "left")
 
 frame2 = Frame(Main, bg = "#FFFFFF")
