ef9787f2189f34f00e2c33ba868076deceafbafa
임성규 <dev.kkyu@gmail.com>
2022-05-24 11:45:09 +0900

    Edit UI

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from tkinter import *
 
 window = Tk()  # Gui 생성
 window.title("Subway")  # 창이름설정
-window.geometry("990x600+100+100")  # 창사이즈설정
+window.geometry("990x630+100+100")  # 창사이즈설정
 window.resizable(False, True)  # 사이즈 변경 허용
 
 # subwayLineButton = []
@@ -22,6 +22,8 @@ class LineNumButton(Button):
       listbox1.delete(0, END)
       for i, x in enumerate(data):
          listbox1.insert(i, x)
+
+      subwaylist.config(text = self.linenum + "호선 역 목록")
 
       lineimage = PhotoImage(file = "./image/"+self.linenum+".png").subsample(16, 16)
       linephoto.config(image = lineimage)
@@ -50,14 +52,16 @@ Main = Frame(frameside, bd=0, bg = "#FFFFFF") # 메인창
 frameside.pack(fill = "both", expand=True) 
 Main.pack(fill = "both", expand=True)
 
+emptyframe0 = Label(Main, bd=0, height = 0, bg = "#FFFFFF")
+emptyframe0.pack(fill = "both")
 
-frame1 = Frame(Main, relief="solid", bd=1)
+frame1 = Frame(Main, bd=0)
 frame1.pack(fill="both")
 
-frame1left = Frame(frame1, relief="solid", bd=1)
+frame1left = Frame(frame1, bd=0)
 frame1left.pack(fill="both", side="left")
 
-frame1right = Frame(frame1, relief="solid", bd=1, bg = "#FFFFFF")
+frame1right = Frame(frame1, bd=0, bg = "#FFFFFF")
 frame1right.pack(fill="both", expand = True, side="left")
 
 Title = Label(frame1left, text="언제타 지하철", font = ("맑은 고딕",44, "bold"), bg = "#FFFFFF", height=1, width=13)
@@ -89,22 +93,33 @@ linephoto = Label(frame2, image = lineimage, bg = "#FFFFFF", padx = 20)
 
 linephoto.pack(fill = "both", side = "right")
 
-frame3 = Frame(Main, relief = "solid", bd=1)
+frame3 = Frame(Main)
 frame3.pack(fill="both", expand = True)
 
-frame3left = Frame(frame3, relief = "solid", bd=1)
-frame3right = Frame(frame3, relief = "solid", bd=1)
-frame3left.pack(fill="both", side = "left")
-frame3right.pack(fill="both", side = "left", expand = True)
+frame3label = Label(frame3, width = 0, bg = "#FFFFFF")
+frame3label.pack(fill = "both", side = "left")
 
-subwaylist = Label(frame3left, text = "호선별 역 목록", font = ("맑은 고딕", 15, "bold"), width = 40, height = 1, relief = "solid", bd=1, bg = "#64C044")
-subwaytime = Label(frame3right, text = "역 열차 시간표", font = ("맑은 고딕", 15, "bold"), height = 1, relief = "solid", bd=1, bg = "#64C044")
+frame3leftout = Frame(frame3, bd=3, bg = "#FFFFFF")
+frame3rightout = Frame(frame3, bd=3, bg = "#FFFFFF")
+frame3leftout.pack(fill="both", side = "left")
+frame3rightout.pack(fill="both", side = "left", expand = True)
+
+frame3label2 = Label(frame3, width = 0, bg = "#FFFFFF")
+frame3label2.pack(fill = "both", side = "right")
+
+frame3left = Frame(frame3leftout, bd=3, bg = "#64C044")
+frame3right = Frame(frame3rightout, bd=3, bg = "#64C044")
+frame3left.pack(fill="both", expand = True)
+frame3right.pack(fill="both", expand = True)
+
+subwaylist = Label(frame3left, text = "호선별 역 목록", font = ("맑은 고딕", 15, "bold"), width = 38, height = 1, bd=0, bg = "#64C044")
+subwaytime = Label(frame3right, text = "역 열차 시간표", font = ("맑은 고딕", 15, "bold"), height = 1, bd=0, bg = "#64C044")
 
 subwaylist.pack(fill = "both")
 subwaytime.pack(fill = "both")
 
-frame3rightup = Frame(frame3right, relief = "solid", bd=1)
-frame3rightdown = Frame(frame3right, relief = "solid", bd=1)
+frame3rightup = Frame(frame3right, bd=0)
+frame3rightdown = Frame(frame3right, bd=0)
 frame3rightup.pack(fill="both", side = "left")
 frame3rightdown.pack(fill="both", side = "left", expand = True)
 
@@ -113,7 +128,10 @@ downlabel = Label(frame3rightdown, text = '하행, 외선', height = 1)
 uplabel.pack(fill = "both")
 downlabel.pack(fill = "both")
 
-listscroll1 = Scrollbar(frame3left)
+frame3leftin = Frame(frame3left, bd=0)
+frame3leftin.pack(fill = "both", expand=True)
+
+listscroll1 = Scrollbar(frame3leftin)
 listscroll2 = Scrollbar(frame3rightup)
 listscroll3 = Scrollbar(frame3rightdown)
 
@@ -121,10 +139,12 @@ listscroll1.pack(fill = "y", side = "right")
 listscroll2.pack(fill = "y", side = "right")
 listscroll3.pack(fill = "y", side = "right")
 
-listbox1=Listbox(frame3left, yscrollcommand = listscroll1.set, font = ('맑은 고딕', 20), height = 1)
+listbox1=Listbox(frame3leftin, yscrollcommand = listscroll1.set, font = ('맑은 고딕', 20), height = 1)
 listbox1.pack(side="left", fill = "both", expand = True)
 
 listscroll1["command"]=listbox1.yview
+
+
 
 def event_for_listbox(event):
    input_text.delete(0, END)
@@ -140,15 +160,15 @@ listbox2.pack(side="left", fill = "both", expand = True)
 
 listscroll2["command"]=listbox2.yview
 
-listbox3=Listbox(frame3rightdown, yscrollcommand = listscroll3.set, font = ('맑은 고딕', 20), height = 1)
+listbox3=Listbox(frame3rightdown, yscrollcommand = listscroll3.set, font = ('맑은 고딕', 20), width = 1, height = 1)
 listbox3.pack(side="left", fill = "both", expand = True)
 
 listscroll3["command"]=listbox3.yview
 
-frame4 = Frame(Main, relief = "solid", bd=1, bg = "#FFFFFF")
+frame4 = Frame(Main, bd=0, bg = "#FFFFFF")
 frame4.pack(fill="both", side = "bottom")
 
-graphframe = Frame(frame4, relief = "solid", bd=1, width = 685, height = 135, bg = "#FFFFFF")
+graphframe = Frame(frame4, bd=0, width = 696, height = 135, bg = "#FFFFFF")
 graphframe.pack(fill = "both", side = "left")
 emailphoto = PhotoImage(file = "./image/email.png").subsample(5, 5)
 mapphoto = PhotoImage(file = "./image/map.png").subsample(5, 5)
