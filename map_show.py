8d5393c1a24f196bb42dbbe964dd163ca54feb92
임성규 <dev.kkyu@gmail.com>
2022-06-06 00:55:48 +0900

    Real Complete Map

--- map_show.py
+++ map_show.py
@@ -1,8 +1,9 @@
 from tkinter import *
 import tkintermapview
 
+
 def mapview(subway):
-    root = Tk() 
+    root = Toplevel()
     root.geometry(f"{800}x{600}") 
     root.title("map_view_example.py") 
 
@@ -14,7 +15,5 @@ def mapview(subway):
     print(marker_1.position, marker_1.text) # get position and text 
     marker_1.set_text(subway) # set new text 
     map_widget.set_zoom(15) # 0~19 (19 is the highest zoom level) 
-
-    root.mainloop()
 
 # mapview("정왕역")
