2d2cc5ad02e79bae3661d6c5f9aec7295c408f53
박자은 <ddoavoca@gmail.com>
2022-06-05 21:13:50 +0900

    Add Map

---
+++ map.py
@@ -0,0 +1,20 @@
+from tkinter import *
+import tkintermapview
+
+def mapview(subway):
+    root = Tk() 
+    root.geometry(f"{800}x{600}") 
+    root.title("map_view_example.py") 
+
+    map_widget = tkintermapview.TkinterMapView(root, width=800, height=500, corner_radius=0) 
+    map_widget.pack()
+    #map_widget.set_position(48.860381, 2.338594) # 위도,경도 위치지정
+    # 주소 위치지정 
+    marker_1 = map_widget.set_address(subway, marker=True)
+    print(marker_1.position, marker_1.text) # get position and text 
+    marker_1.set_text(subway) # set new text 
+    map_widget.set_zoom(15) # 0~19 (19 is the highest zoom level) 
+
+    root.mainloop()
+
+mapview("정왕역")
