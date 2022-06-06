a1d019b5def7d1067be7adba2383333c8c68c159
박자은 <ddoavoca@gmail.com>
2022-06-07 04:39:52 +0900

    70% Complete

--- games/YahtZee/yahtzeeboard.py
+++ games/YahtZee/yahtzeeboard.py
@@ -103,6 +103,10 @@ class YahtzeeBoard:
     def rollDiceListener(self):
         # 'state' 값이 'disabled'가 아닌 모든 주사위 값을 새로 할당하고 화면에 표시.
         # TODO: 구현
+        for i, x in enumerate(self.diceButtons):
+            if x['state'] != 'disabled':
+                self.dice[i].rollDie()
+                x.config(text = self.dice[i].getRoll())
 
         # self.roll 이 0, 1 일 때 : 
         if (self.roll == 0 or self.roll ==1):
@@ -113,69 +117,287 @@ class YahtzeeBoard:
             self.bottomLabel.configure(text="카테고리를 선택하세요")
             self.rollDice['state'] = 'disabled'
             self.rollDice['bg'] = 'light gray'
-            self.roll = 0
+            # self.roll = 0
 
     # 각 주사위에 해당되는 버튼 클릭 : disable 시키고 배경색을 어둡게 바꿔 표현해 주기.
     def diceListener(self, row):
-        self.diceButtons[row]['state'] = 'disabled'
-        self.diceButtons[row]['bg'] = 'light gray'
+        if self.roll != 0:
+            self.diceButtons[row]['state'] = 'disabled'
+            self.diceButtons[row]['bg'] = 'light gray'
 
     # 카레고리 버튼 눌렀을 때의 처리.
     #   row: 0~5, 8~14
     def categoryListener(self, row):
-        score = Configuration.score(row, self.dice)      #점수 계산
-        # index : 0~12
-        index = row
-        if (row>7):
-            index = row-2
-        cur_player = self.players[self.player]
-
-        # (1) cur_player에 setScore(), setAtUsed() 호출하여 점수와 사용상태 반영.
-        # (2) 선택한 카테고리의 점수를 버튼에 적고 
-        # (3) 버튼을 disable 시킴.
-        # TODO: 구현
-        
-        # UPPER category가 전부 사용되었으면(cur_player.allUpperUsed()로써 확인)
-        # -> cur_player.getUpperScore() 점수에 따라
-        #    UI의 UPPERTOTAL, UPPERBONUS 에 내용 채우기.
-        # TODO: 구현
-
-        # LOWER category 전부 사용되었으면(cur_player.allLowerUsed()로써 확인) 
-        # -> cur_player.getLowerScore() 점수에 따라
-        #   UI의 LOWERTOTAL 에 내용 채우기.
-        # TODO: 구현
+        if self.roll != 0:
+            score = Configuration.score(row, self.dice)      #점수 계산
+            # index : 0~12
+            index = row
+            if (row>7):
+                index = row-2
+            cur_player = self.players[self.player]
+    
+            # (1) cur_player에 setScore(), setAtUsed() 호출하여 점수와 사용상태 반영.
+            # (2) 선택한 카테고리의 점수를 버튼에 적고 
+            # (3) 버튼을 disable 시킴.
+            # TODO: 구현
+            for num in range(6):
+                if index == num:
+                    if  cur_player.used[index] == False:
+                        temp = 0
+                        for i in range(5):
+                            if self.dice[i].getRoll() == num+1:
+                                temp += num+1
+                        cur_player.setScore(temp, index)
+                        cur_player.setAtUsed(index)
+                        self.fields[row][self.player].config(text = temp)
+                        self.fields[row][self.player]['state'] = 'disabled'
+                    else:
+                        return
             
-        # UPPER category와 LOWER category가 전부 사용되었으면 
-        # -> UI의 TOTAL 에 내용 채우기.
-        # TODO: 구현
+            if index == 6:
+                if  cur_player.used[index] == False:
+                    num = [0 for i in range(6)]
+                    for i in range(5):
+                        temp = self.dice[i].getRoll()
+                        for j in range(6):
+                            if temp == j+1:
+                                num[j] += 1
+                                break
 
-        # 다음 플레이어로 가기.
-        self.player = (self.player + 1) % self.numPlayers
+                    for i in range(6):
+                        if num[i] >= 3:
+                            temp = 0
+                            for i in range(5):
+                                temp += self.dice[i].getRoll()
+                            cur_player.setScore(temp, index)
+                            cur_player.setAtUsed(index)
+                            self.fields[row][self.player].config(text = temp)
+                            self.fields[row][self.player]['state'] = 'disabled'
+                            break
+                    else:
+                        cur_player.setScore(0, index)
+                        cur_player.setAtUsed(index)
+                        self.fields[row][self.player].config(text = 0)
+                        self.fields[row][self.player]['state'] = 'disabled'
+                else:
+                    return
+                        
+                
+            
+            elif index == 7:
+                if  cur_player.used[index] == False:
+                    num = [0 for i in range(6)]
+                    for i in range(5):
+                        temp = self.dice[i].getRoll()
+                        for j in range(6):
+                            if temp == j+1:
+                                num[j] += 1
+                                break
 
-        # 선택할 수 없는 카테고리들과 현재 player 것이 아닌 버튼들은 disable 시키기.
-        # 그 외는 enable 시키기.
-        # TODO: 구현
+                    for i in range(6):
+                        if num[i] >= 4:
+                            temp = 0
+                            for i in range(5):
+                                temp += self.dice[i].getRoll()
+                            cur_player.setScore(temp, index)
+                            cur_player.setAtUsed(index)
+                            self.fields[row][self.player].config(text = temp)
+                            self.fields[row][self.player]['state'] = 'disabled'
+                            break
+                    else:
+                        cur_player.setScore(0, index)
+                        cur_player.setAtUsed(index)
+                        self.fields[row][self.player].config(text = 0)
+                        self.fields[row][self.player]['state'] = 'disabled'
+                else:
+                    return
 
-        # 라운드 증가 시키기.
-        if self.player == 0:
-            self.round += 1
 
-        # 게임이 종료되었는지 검사 (13 round의 마지막 플레이어일 때) 
-        # -> 이긴 사람을 알리고 새 게임 시작.
-        # TODO: 구현
+            elif index == 8:
+                if  cur_player.used[index] == False:
+                    num = [0 for i in range(6)]
+                    for i in range(5):
+                        temp = self.dice[i].getRoll()
+                        for j in range(6):
+                            if temp == j+1:
+                                num[j] += 1
+                                break
+                    check = 1
+                    for i in range(6):
+                        if num[i] == 3:
+                            for j in range(6):
+                                if num[j] == 2:
+                                    cur_player.setScore(25, index)
+                                    cur_player.setAtUsed(index)
+                                    self.fields[row][self.player].config(text = 25)
+                                    self.fields[row][self.player]['state'] = 'disabled'
+                                    check = 0
+                                    break
+                            break
+                    if check:
+                        cur_player.setScore(0, index)
+                        cur_player.setAtUsed(index)
+                        self.fields[row][self.player].config(text = 0)
+                        self.fields[row][self.player]['state'] = 'disabled'
+                else:
+                    return
 
-        # 다시 Roll Dice 버튼과 diceButtons 버튼들을 활성화.
-        self.rollDice.configure(text="Roll Dice")
-        self.rollDice['state'] = 'normal'
-        self.rollDice['bg'] = self.color_btn_bg
-        for i in range(5):  #dice 버튼 5개 생성
-            self.diceButtons[i].configure(text='')
-            self.diceButtons[i]['state'] = 'normal'
-            self.diceButtons[i]['bg'] = self.color_btn_bg
+            
+            elif index == 9:
+                if  cur_player.used[index] == False:
+                    num = [0 for i in range(6)]
+                    for i in range(5):
+                        temp = self.dice[i].getRoll()
+                        for j in range(6):
+                            if temp == j+1:
+                                num[j] += 1
+                                break
+                    cnt1 = cnt2 = cnt3 = 0
+                    for i in range(4):
+                        if num[i] != 0:
+                            cnt1 += 1
+                        if num[i+1] != 0:
+                            cnt2 += 1
+                        if num[i+2] != 0:
+                            cnt3 += 1
+                    if cnt1 == 4 or cnt2 == 4 or cnt3 == 4:
+                        cur_player.setScore(30, index)
+                        cur_player.setAtUsed(index)
+                        self.fields[row][self.player].config(text = 30)
+                        self.fields[row][self.player]['state'] = 'disabled'
+                    else:
+                        cur_player.setScore(0, index)
+                        cur_player.setAtUsed(index)
+                        self.fields[row][self.player].config(text = 0)
+                        self.fields[row][self.player]['state'] = 'disabled'
+                else:
+                    return
+            
 
-        # bottomLabel 초기화.
-        self.bottomLabel.configure(text=cur_player.toString()+
-            "차례: Roll Dice 버튼을 누르세요")
+            elif index == 10:
+                if  cur_player.used[index] == False:
+                    num = [0 for i in range(6)]
+                    for i in range(5):
+                        temp = self.dice[i].getRoll()
+                        for j in range(6):
+                            if temp == j+1:
+                                num[j] += 1
+                                break
+                    cnt1 = cnt2 = 0
+                    for i in range(5):
+                        if num[i] != 0:
+                            cnt1 += 1
+                        if num[i+1] != 0:
+                            cnt2 += 1
+                    if cnt1 == 5 or cnt2 == 5:
+                        cur_player.setScore(40, index)
+                        cur_player.setAtUsed(index)
+                        self.fields[row][self.player].config(text = 40)
+                        self.fields[row][self.player]['state'] = 'disabled'
+                    else:
+                        cur_player.setScore(0, index)
+                        cur_player.setAtUsed(index)
+                        self.fields[row][self.player].config(text = 0)
+                        self.fields[row][self.player]['state'] = 'disabled'
+                else:
+                    return
+
+            elif index == 11:
+                if  cur_player.used[index] == False:
+                    num = [0 for i in range(6)]
+                    for i in range(5):
+                        temp = self.dice[i].getRoll()
+                        for j in range(6):
+                            if temp == j+1:
+                                num[j] += 1
+                                break
+                    for i in range(6):
+                        if num[i] == 5:
+                            cur_player.setScore(50, index)
+                            cur_player.setAtUsed(index)
+                            self.fields[row][self.player].config(text = 50)
+                            self.fields[row][self.player]['state'] = 'disabled'
+                            break
+                    else:
+                        cur_player.setScore(0, index)
+                        cur_player.setAtUsed(index)
+                        self.fields[row][self.player].config(text = 0)
+                        self.fields[row][self.player]['state'] = 'disabled'
+                else:
+                    return
+            
+            elif index == 12:
+                if  cur_player.used[index] == False:
+                    temp = 0
+                    for i in range(5):
+                        temp += self.dice[i].getRoll()
+
+                    cur_player.setScore(temp, index)
+                    cur_player.setAtUsed(index)
+                    self.fields[row][self.player].config(text = temp)
+                    self.fields[row][self.player]['state'] = 'disabled'
+                else:
+                    return
+
+
+            
+            # UPPER category가 전부 사용되었으면(cur_player.allUpperUsed()로써 확인)
+            # -> cur_player.getUpperScore() 점수에 따라
+            #    UI의 UPPERTOTAL, UPPERBONUS 에 내용 채우기.
+            # TODO: 구현
+            if cur_player.allUpperUsed():
+                self.fields[6][self.player].config(text = cur_player.getUpperScore())
+                if cur_player.getUpperScore() >= 63:
+                    self.fields[7][self.player].config(text = 35)
+    
+            # LOWER category 전부 사용되었으면(cur_player.allLowerUsed()로써 확인) 
+            # -> cur_player.getLowerScore() 점수에 따라
+            #   UI의 LOWERTOTAL 에 내용 채우기.
+            # TODO: 구현
+                
+            # UPPER category와 LOWER category가 전부 사용되었으면 
+            # -> UI의 TOTAL 에 내용 채우기.
+            # TODO: 구현
+    
+            # 다음 플레이어로 가기.
+            self.player = (self.player + 1) % self.numPlayers
+    
+            # 선택할 수 없는 카테고리들과 현재 player 것이 아닌 버튼들은 disable 시키기.
+            # 그 외는 enable 시키기.
+            # TODO: 구현
+    
+            # 라운드 증가 시키기.
+            if self.player == 0:
+                self.round += 1
+    
+            # 게임이 종료되었는지 검사 (13 round의 마지막 플레이어일 때) 
+            # -> 이긴 사람을 알리고 새 게임 시작.
+            # TODO: 구현
+    
+            # 다시 Roll Dice 버튼과 diceButtons 버튼들을 활성화.
+            self.rollDice.configure(text="Roll Dice")
+            self.rollDice['state'] = 'normal'
+            self.rollDice['bg'] = self.color_btn_bg
+            for i in range(5):  #dice 버튼 5개 생성
+                self.diceButtons[i].configure(text='')
+                self.diceButtons[i]['state'] = 'normal'
+                self.diceButtons[i]['bg'] = self.color_btn_bg
+    
+            # bottomLabel 초기화.
+            self.bottomLabel.configure(text=self.players[self.player].toString()+
+                "차례: Roll Dice 버튼을 누르세요")
+            for i in range(self.TOTAL + 2):  # i행 : 점수
+                for j in range(self.numPlayers):  # j열 : 플레이어
+                    if (i != 0):
+                        if (j != self.player or (i-1) == self.UPPERTOTAL or (i-1) == self.UPPERBONUS 
+                            or (i-1) == self.LOWERTOTAL or (i-1) == self.TOTAL):
+                            self.fields[i-1][j]['state'] = 'disabled'
+                            self.fields[i-1][j]['bg'] = 'light gray'
+                        else:
+                            self.fields[i-1][j]['state'] = 'normal'
+                            self.fields[i-1][j]['bg'] = self.color_btn_bg
+            self.roll = 0
+            
 
 if __name__ == '__main__':
     YahtzeeBoard()
