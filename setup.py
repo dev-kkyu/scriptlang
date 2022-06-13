6a72d554d72c6f73e8e1bea059935eee423271c7
임성규 <dev.kkyu@gmail.com>
2022-06-13 23:11:06 +0900

    Update Distribute File

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from distutils.core import setup
 setup(
 name='Subway', 
 version='1.0', # 파일명이 아니라 모듈명을 입력 (.py라는 확장자는 제외)
-py_modules=['Subway'], 
+py_modules=['Subway', 'Telegram_bot'], 
 packages=['image', 'modules'], 
 package_data = {'image': ['*.png'], 'modules': ['*.py*']}, )
