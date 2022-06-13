71ed1c5b84c5cc8c548b3cd9565e5f609abfc861
임성규 <dev.kkyu@gmail.com>
2022-06-13 18:52:02 +0900

    Change Mail with HTML

--- modules/email_send.py
+++ modules/email_send.py
@@ -6,7 +6,47 @@ def sendMail(fromAddr, toAddr, code):
     import smtplib # 파이썬의 SMTP 모듈# 메일 서버와 connect하고 통신 시작
     maildata = Code2Timetable(code)
 
-    msg = MIMEText('상행, 내선 \n'+str(maildata['Schedule']['1'])+'\n하행, 외선\n'+ str(maildata['Schedule']['2'])) 
+    datainfo = maildata['LINE_NUM'] + ' ' + maildata['STATION_NM'] + '역 검색 결과'
+    schedule1 = ''
+    schedule2 = ''
+    for i in maildata['Schedule']['1']:
+        schedule1 += (i + '\n') 
+    
+    for i in maildata['Schedule']['2']:
+        schedule2 += (i + '\n') 
+
+    html = '''
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            지하철 시간표
+        </title>
+    </head>
+    <body>
+        <h1>
+            언제타 지하철
+        </h1>
+        <h2>
+''' + datainfo + '''
+        </h2>
+        <h3>
+            상행
+        </h3>
+        <h4>
+''' + schedule1 + '''
+        </h4>
+        <h3>
+            하행
+        </h3>
+        <h4>
+''' + schedule2 + '''
+        </h4>
+    </body>
+</html>
+'''
+    msg = MIMEText(html, 'html')
+
     msg['Subject'] = maildata['STATION_NM']+'역 시간표' 
 
     msg['From'] = fromAddr 
@@ -18,4 +58,4 @@ def sendMail(fromAddr, toAddr, code):
     s.login('jjaeunjj@gmail.com', 'zkedcjmpqmhmiuav')  # 앱 password 이용
     s.sendmail(fromAddr , [toAddr], msg.as_string()) 
     s.close() 
-       
+    
